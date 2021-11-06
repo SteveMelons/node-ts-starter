906fa0a9dc1d5f122525715a44e000e5b9a477dd
SteveMelons <oli.olioli.fischer@gmail.com>
2021-11-06 19:42:52 +0100

    moved logger to seperated file and exported

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# node-ts-starter
+# printer-testing
 
 Includes typescript, .env loading, configured winston logger and scripts to build and run in development mode
--- src/index.ts
+++ src/index.ts
@@ -1,40 +1,5 @@
 import "dotenv-safe/config";
-import winston from "winston";
-
-/* logging setup */
-const winstonColorConfig = {
-  levels: {
-    error: 0,
-    debug: 1,
-    warn: 2,
-    data: 3,
-    info: 4,
-  },
-  colors: {
-    error: "red",
-    debug: "blue",
-    warn: "yellow",
-    data: "cyan",
-    info: "green",
-  },
-};
-winston.addColors(winstonColorConfig.colors);
-const logConfiguration = {
-  levels: winstonColorConfig.levels,
-  transports: [new winston.transports.Console()],
-  format: winston.format.combine(
-    winston.format.colorize(),
-    winston.format.timestamp({
-      format: "MMM-DD-YYYY HH:mm:ss.SSS",
-    }),
-    winston.format.align(),
-    winston.format.printf(
-      (info) =>
-        `\x1b[2m[${info.timestamp}]\x1b[0m [${info.level}] ${info.message}`
-    )
-  ),
-};
-const logger = winston.createLogger(logConfiguration);
+import { logger } from "./logger";
 
 /* main */
 (async () => {
